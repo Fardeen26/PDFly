1a649df2a4e48ef9613c1052271c3cf145675455
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-12-19 17:08:10 +0530

    feat: initialized tailwind css

--- src/components/ListContainer.tsx
+++ src/components/ListContainer.tsx
@@ -3,6 +3,6 @@ import { pdfType } from "../types/pdf";
 export default function ListContainer({ pdfs }: { pdfs: pdfType[] }) {
     console.log(pdfs)
     return (
-        <p>List Container</p>
+        <div className="text-red-500">hello</div>
     )
 }
